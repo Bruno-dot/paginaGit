9c7a8c6c68630d765215561a6673f4acaf443aff
Bruno <viniciusbruno822@gmail.com>
2021-09-30 22:45:23 -0300

    teste em cmaismais

---
+++ teste1.cpp
@@ -0,0 +1,36 @@
+//primeiro teste em C++
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int iniciador (){
+
+    cout << "O programa deu certo, parabens eu sei funcao";
+    return 0;
+} 
+
+
+int main(int argc, char const *argv[])
+{
+
+    string texto = "Estou escrevendo uma string que legal \n";
+
+
+//novidade de c++
+
+        cout <<"Iniciando programa \n";
+        cout << texto;
+        system("pause");
+        iniciador();
+
+
+
+//funçoes do C
+    printf("teste do printf \n");
+
+
+    return 0;
+}
